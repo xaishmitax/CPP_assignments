c02c58b3ac5762af349e6cc8be159f091ed7761b
AIshmita Yonzon <aishmitayonzon04@gmail.com>
2025-01-28 10:38:32 +0545

    added a commit file

---
+++ add.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Declare variables to store two numbers
+    int num1, num2, sum;
+
+    // Input two numbers
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    // Calculate the sum
+    sum = num1 + num2;
+
+    // Output the result
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
